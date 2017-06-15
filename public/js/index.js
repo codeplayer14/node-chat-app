2be13f7a2a4dba41ddc2711253b9cbf55d8180d4
iPaddy <padmanabh14@gmail.com>
2017-06-15 23:04:27 +0530

    New message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+var socket = io();
+
+  socket.on('connect',function(){
+    console.log('Connected to server.');
+  });
+
+  socket.on('disconnect',function(){
+
+    console.log('Disconnected from server');
+
+  });
+
+
+  socket.on('newEmail',function(data){
+
+    console.log('New Email.',data);
+
+  });
+
+socket.emit('createEmail',{
+
+    from: 'Paddy',
+    text: 'Did it work?'
+});
+
+socket.on('newMessage',function(data){
+
+  console.log('New message: ',data);
+
+});
+
+socket.emit('createMessage',{
+  from:'XYZ',
+  text:'Sure. Done scenes.'
+});
--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const http = require('http');
 const socketIO = require('socket.io');
 const port = process.env.PORT||3000;
 const express = require('express');
-
 const publicPath = path.join(__dirname,'../public');
 
 var app = express();
@@ -19,8 +18,35 @@ io.on('connection',(socket)=>{
   socket.on('disconnect',(socket)=>{
 
       console.log('Disconnected.');
+
+    });
+
+  socket.emit('newMessage',{
+
+      from: 'Paddy',
+      text: 'Hey, when do you want to meet',
+      createdAt: +new Date()
   });
+
+  socket.on('createMessage',function(message){
+
+      console.log('New Message received: ',message);
+  })
+  socket.emit('newEmail',{
+
+    from:'paddy@gmail.com',
+    text:'Wassup nigga.',
+    createdAt: 123
+  });
+
+  socket.on('createEmail',(newEmail)=>{
+
+    console.log('Got a mail: ',newEmail);
+
+  })
+
 });
+
 
 
 server.listen(port,()=>{
